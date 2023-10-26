70ff89e6d2e739d35c4edda87a79350dd96d5a20
Luis Felipe Fabiane <luisffabiane@gmail.com>
2023-10-25 21:48:45 -0300

    Update analisador_lexico.py

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,14 +1,14 @@
 alfabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
-number = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
 
 
-acceptedChars = alfabet.union(number)
+acceptedChars = alfabet.union(numbers)
 
 
 lista_de_palavras_reservadas = {'def', 'int', 'float', 'string', 'break', 'print', 'read', 'return', 'if', 'else', 'for', 'new', 'null'}
 
 
-lista_de_simbolos = {'(', ')', '{', '}', ';', '<', '>', '=', '!', '[', ']', '*', '+', '-', '/', '%'}
+lista_de_simbolos = {'(', ')', '{', '}', ';', '<', '>', '=', '!', '[', ']', '*', '+', '-', '/', '%', '"'}
 
 
 lista_de_tokens = []
@@ -17,7 +17,7 @@ lista_de_tokens = []
 tabela_de_simbolos = []
 
 
-
+haveError = False
 
 
 
@@ -49,68 +49,98 @@ def analisador_ident(charlist, init, linha):
         return False, init
 
 
-    ident = charlist[init]
+    token = charlist[init]
 
 
     pointer = init + 1
     while(charlist[pointer] in acceptedChars):
-        ident += charlist[pointer]
+        token += charlist[pointer]
         pointer+=1
-    if ident in lista_de_palavras_reservadas:
-        lista_de_tokens.append(ident)
+    if token in lista_de_palavras_reservadas:
+        lista_de_tokens.append(token)
     else:
         lista_de_tokens.append('ident')
         inList = False
         for linhaMatriz in tabela_de_simbolos:
-            if ident == linhaMatriz[0]:
+            if token == linhaMatriz[0]:
                 inList = True
                 linhaMatriz.append(linha)
                 break
         if not inList:
-            tabela_de_simbolos.append([ident, linha])
+            tabela_de_simbolos.append([token, linha])
     end = pointer
     return True, end
 
+def analisador_number_constant(charlist, init):
+    if (charlist[init] not in numbers):
+        return False, init
+    
+    token = charlist[init]
+    pointer = init + 1
+    while(charlist[pointer] in numbers ):
+        token += charlist[pointer]
+        pointer+=1
+    if charlist[pointer] == '.':
+        pointer+=1
+        while(charlist[pointer] in numbers ):
+            token += charlist[pointer]
+            pointer+=1
+        lista_de_tokens.append('float_constant')
+    else:
+        lista_de_tokens.append('int_constant')
 
+    end = pointer
+    return True, end
 
-
-
-
+def analisador_string_constant(charlist, init):
+    if (charlist[init] != '"'):
+        return False, init,
+    return True, init, 
 
 
 def analisador(charlist):
     linha = 1
     pointer = 0
+    isIdent = False
+    isNumber = False
+    isString = False
+    isSimbol = False
     while(1):
         try:
             char = charlist[pointer]
         except:
-            break
-
-
-
+            return False
 
         isIdent, pointer = analisador_ident(charlist, pointer, linha)
-        if(isIdent):
-            print('IDENT ', end='')
-        elif (char == ' ' or char == '\n'):
-            print(char, end='')
-            if char == '\n':
-                linha+=1
+
+        if not isIdent:
+            isNumber, pointer = analisador_number_constant(charlist, pointer)
+            if not isNumber:
+                isString, pointer = analisador_string_constant(charlist, pointer)
+                if not isString:
+                    isSimbol = (charlist[pointer] in lista_de_simbolos)
+                    pointer += 1
+                    if isSimbol:
+                        lista_de_tokens.append(charlist[pointer])
+        
+        if char == ' ':
             pointer+=1
-        else:
-            lista_de_tokens.append(char)
-            print('OUTRO ', end=' ')
+        if char == '\n':
+            linha+=1
             pointer+=1
+        if isIdent and not isNumber and not isString and not isSimbol and charlist[pointer] != ' ' and charlist[pointer] != '\n':
+            print(isIdent, isNumber, isSimbol, isString)
+            print(charlist[pointer])
+            print('Erro Léxico na linha '+ str(linha))
+            return True
 
+haveError = analisador(codigo)
 
-
-
-analisador(codigo)
-print()
-print(lista_de_tokens)
-print()
-print(tabela_de_simbolos)
+if not haveError:
+    print()
+    print(lista_de_tokens)
+    print()
+    print(tabela_de_simbolos)
 
 
 
@@ -144,5 +174,3 @@ print(tabela_de_simbolos)
 #     print(f"File '{file_path}' not found.")
 # except Exception as e:
 #     print(f"An error occurred: {e}")
-
-
